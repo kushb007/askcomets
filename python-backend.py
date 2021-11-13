9c81a286e90ade0d6e250e37be8b24e8e18e3050
Abrar M Zaman <38172127+Slowpok3@users.noreply.github.com>
2021-11-13 16:19:48 -0600

    Add files via upload

--- python-backend.py
+++ python-backend.py
@@ -8,7 +8,7 @@ api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
 db = SQLAlchemy(app)
 
-QUESTIONID = 0;
+
 
 class postModel(db.Model):
 	postId = db.Column(db.Integer, primary_key = True, nullable=False)
@@ -28,7 +28,7 @@ class answerModel(db.Model):
 
 
 
-# db.create_all()
+#db.create_all()
 getQuestionArgs = reqparse.RequestParser()
 
 getQuestionArgs.add_argument("userName" , type=str)
@@ -41,6 +41,13 @@ question_put_args.add_argument("Title", type=str, help="Add Title: ")
 question_put_args.add_argument("Content", type=str, help="Add Content: ")
 question_put_args.add_argument("Class", type=str, help="Add Class: ")
 
+getAnswerArgs = reqparse.RequestParser()
+getAnswerArgs.add_argument("postid" , type=int)
+
+answer_put_args = reqparse.RequestParser()
+answer_put_args.add_argument("answerid", type=int)
+answer_put_args.add_argument("answerbody", type=str)
+answer_put_args.add_argument("postid", type=str)
 resource_fields = {
     'postId' : fields.Integer,
     'userName' : fields.String,
@@ -52,22 +59,60 @@ resource_fields = {
     'numViews' : fields.Integer,
 }
 
+resource_fields2 = {
+    'answerId' : fields.Integer,
+    'postId' : fields.Integer,
+    'answerBody' : fields.String
+}
+
 class Questions (Resource):
- 	
+	QUESTIONID = 0
+
+	@marshal_with(resource_fields)
 	def get(self):
 		args = getQuestionArgs.parse_args()
 		result = postModel.query.filter_by(postTitle=args['titleQuery']).first()
 		return result
 
+	@marshal_with(resource_fields)
 	def put (self):
 		args = question_put_args.parse_args()
-		question = postModel(postId = QUESTIONID, userName=args['useame'], postTitle=args['Title'], postBody=args['Content'], className=args['Class'], resolved=False, numViews=0  )
-		QUESTIONID = QUESTIONID + 1
+
+		print(str(Questions.QUESTIONID))
+		
+
+		result = postModel.query.filter_by(postId=Questions.QUESTIONID).first()
+		if (result):
+			abort(409, message="post already exists")
+
+		question = postModel(postId = Questions.QUESTIONID, userName=args['name'], postTitle=args['Title'], postBody=args['Content'], className=args['Class'], resolved=False, numViews=0  )
+		Questions.QUESTIONID = Questions.QUESTIONID + 1
+
 		db.session.add(question)
 		db.session.commit()
 		return question, 201
 
 api.add_resource(Questions, "/Questions")
 
-if __name__ == " __main__ " :
+class Answers (Resource):
+    ANSWERID = 8
+    @marshal_with(resource_fields2)
+    def get(self):
+        args = getAnswerArgs.parse_args()
+        result = answerModel.query.filter_by(postId=args['postId']).first()
+        return result
+
+    @marshal_with(resource_fields2)
+    def put(self):
+        args = answer_put_args.parse_args()
+        answer = answerModel(answerId = Answers.ANSWERID, answerBody=args['answerbody'], postId=args['postid'])
+        Answers.ANSWERID = Answers.ANSWERID + 1
+        db.session.add(answer)
+        db.session.commit()
+        return answer, 201
+api.add_resource(Answers, "/Answers")
+
+
+
+if __name__ == "__main__" :
 	app.run(debug=True)
